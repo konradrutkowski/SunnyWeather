40a1dde8b97997e4807cb6147e7a0195b96b1074
Konrad <konradxrutkowski@gmail.com>
2017-07-20 21:41:29 +0200

    Small improvements

--- app/src/main/java/com/pjkr/sunnyweather/longterm/view/LongTermHolder.kt
+++ app/src/main/java/com/pjkr/sunnyweather/longterm/view/LongTermHolder.kt
@@ -14,5 +14,7 @@ class LongTermHolder(view: View) : RecyclerView.ViewHolder(view) {
     var titleTV: TextView = view.findViewById(R.id.title_long_term_row) as TextView
     var descriptionTV: TextView = view.findViewById(R.id.description_long_term_row) as TextView
     var day: TextView = view.findViewById(R.id.day_tv) as TextView
+    var icon: TextView = view.findViewById(R.id.day_tv) as TextView
+
 
 }
